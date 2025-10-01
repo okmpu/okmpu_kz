77a0242a5862cf5f948e84172a9a8f934f0c8ff6
mukhagaliaryn <mukhagaliaryn@gmail.com>
2025-10-01 18:11:14 +0500

    Done

--- apps/home/views/public.py
+++ apps/home/views/public.py
@@ -1,42 +1,53 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 from register.models.publics import Public
 
 
-# News pages
+# news page
+# ----------------------------------------------------------------------------------------------------------------------
 def news_view(request, pk=None):
     if pk:
         item = get_object_or_404(Public, pk=pk, public_type='news')
-        similar_items = Public.objects.exclude(pk=item.pk, public_type='news')[:5]
+        similar_items = Public.objects.exclude(pk=item.pk, public_type='news')[:10]
         context = {
             'code': 'news',
             'item': item,
             'similar_items': similar_items
         }
-        return render(request, 'app/publics/public_detail.html', context)
+        return render(request, 'app/publics/public/page.html', context)
     else:
         items = Public.objects.filter(public_type='news')
+        paginator = Paginator(items, 100)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
         context = {
             'code': 'news',
-            'items': items
+            'page_obj': page_obj
         }
-        return render(request, 'app/publics/index.html', context)
+        return render(request, 'app/publics/page.html', context)
 
 
-# Announcement pages
+# announcement page
+# ----------------------------------------------------------------------------------------------------------------------
 def announcement_view(request, pk=None):
     if pk:
         item = get_object_or_404(Public, pk=pk, public_type='ann')
-        similar_items = Public.objects.exclude(pk=item.pk, public_type='ann')[:5]
+        similar_items = Public.objects.exclude(pk=item.pk, public_type='ann')[:10]
         context = {
             'code': 'announcement',
             'item': item,
             'similar_items': similar_items
         }
-        return render(request, 'app/publics/public_detail.html', context)
+        return render(request, 'app/publics/public/page.html', context)
     else:
         items = Public.objects.filter(public_type='ann')
+        paginator = Paginator(items, 100)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
         context = {
             'code': 'announcement',
-            'items': items
+            'page_obj': page_obj
         }
-        return render(request, 'app/publics/index.html', context)
+        return render(request, 'app/publics/page.html', context)
